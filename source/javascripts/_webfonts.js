294dd6d635ce40c6247f02319a1b91f11f435041
Rob Fletcher <rfletcher@netflix.com>
2014-07-11 07:09:28 +0100

    grid fixes, etc

--- source/javascripts/_webfonts.js
+++ source/javascripts/_webfonts.js
@@ -1,5 +1,5 @@
 WebFontConfig = {
-  google: { families: [ 'Maiden+Orange::latin', 'Oswald::latin', 'Francois+One::latin' ] }
+  google: { families: [ 'Maiden+Orange::latin', 'Oswald::latin' ] }
 };
 (function() {
   var wf = document.createElement('script');
